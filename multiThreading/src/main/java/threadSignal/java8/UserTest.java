32ff514b28cac8d0630911b438a6ed6e06ea16db
Jialei Liu <jialei.liu@microfocus.com>
2023-03-15 13:45:34 +0800

    Optional

---
+++ multiThreading/src/main/java/threadSignal/java8/UserTest.java
@@ -0,0 +1,26 @@
+package threadSignal.java8;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.Optional;
+
+public class UserTest {
+    static Gson gson = new Gson();
+    static String userJson = "{\"data\":{\"node\":true}}";
+    private static final String DATA = "data";
+
+
+    public static void main(String[] args) {
+        String deleteEntityStr = "node";
+        JsonObject jsonObject = gson.fromJson(userJson, JsonObject.class);
+        boolean isSuccess = Optional.ofNullable(jsonObject)
+                .map(o -> jsonObject.get(DATA))
+                .map(JsonElement::getAsJsonObject)
+                .map(o -> o.get(deleteEntityStr))
+                .filter(r -> !r.isJsonNull() && r.getAsBoolean())
+                .isPresent();
+        System.out.println(isSuccess);
+    }
+}
